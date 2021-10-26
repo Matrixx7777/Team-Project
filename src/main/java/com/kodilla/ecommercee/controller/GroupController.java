8063812777f916ca1b7d9d68ae3c3d402cc9213b
Dawid Kocik <kocik.dawid@wp.pl>
2021-10-26 21:44:12 +0200

    I created the groupController and GroupDto on 26.10.2021

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -1,4 +1,38 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.dto.GroupDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/group")
 public class GroupController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getGroups")
+    public List<GroupDto> getGroups() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getGroup")
+    public GroupDto getGroup (@RequestParam int groupId) {
+        return new GroupDto(new BigInteger(String.valueOf(1)),"Fruit");
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createGroup")
+    public GroupDto createGroup(@RequestBody GroupDto groupDto) {
+        return groupDto;
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateGroup")
+    public GroupDto updateGroup(@RequestParam int groupId) {
+        return new GroupDto(new BigInteger(String.valueOf(1)), "Fruit");
+    }
+
+    @RequestMapping( method = RequestMethod.DELETE, value = "deleteProduct")
+    public void deleteProduct(@RequestParam int productId) {
+    }
+
 }
