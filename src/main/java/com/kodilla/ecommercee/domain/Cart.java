adee85dc0ebc2d33cfec559c9ec28ae8b780c974
huubsoon <hubertskorulski@gmail.com>
2021-10-29 11:30:08 +0200

    Dodalem repozytorium, przenioslem adnotacje na zmienne

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -9,9 +9,27 @@ import java.util.List;
 @Table(name="CARTS")
 public class Cart {
 
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name="CART_ID", unique = true)
     private Long id;
+
+    @ManyToOne
+    @NotNull
+    @JoinColumn(name = "USER_ID")
     private User user;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "ORDER_ID")
     private Order order;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name= "CARTS_HAS_PRODUCTS",
+            joinColumns = {@JoinColumn(name = "CART_ID", referencedColumnName = "CART_ID")},
+            inverseJoinColumns = {@JoinColumn(name = "PRODUCT_ID", referencedColumnName = "PRODUCT_ID")}
+    )
     private List<Product> products = new ArrayList<>();
 
     public Cart(User user, Order order) {
@@ -22,51 +40,5 @@ public class Cart {
     public Cart() {
     }
 
-    @Id
-    @GeneratedValue
-    @NotNull
-    @Column(name="CART_ID", unique = true)
-    public Long getId() {
-        return id;
-    }
-
-    @ManyToOne
-    @NotNull
-    @JoinColumn(name = "USER_ID")
-    public User getUser() {
-        return user;
-    }
-
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name = "ORDER_ID")
-    public Order getOrder() {
-        return order;
-    }
-
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(
-            name= "CARTS_HAS_PRODUCTS",
-            joinColumns = {@JoinColumn(name = "CART_ID", referencedColumnName = "CART_ID")},
-            inverseJoinColumns = {@JoinColumn(name = "PRODUCT_ID", referencedColumnName = "PRODUCT_ID")}
-    )
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    private void setId(Long id) {
-        this.id = id;
-    }
-
-    private void setUser(User user) {
-        this.user = user;
-    }
-
-    private void setOrder(Order order) {
-        this.order = order;
-    }
-
-    private void setProducts(List<Product> products) {
-        this.products = products;
-    }
 }
 
