8938e73bb30e2927dbf63972d6c0a5c88fc1419e
Dawid Kocik <kocik.dawid@wp.pl>
2021-10-31 19:17:44 +0100

    New Class Group and Product

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
