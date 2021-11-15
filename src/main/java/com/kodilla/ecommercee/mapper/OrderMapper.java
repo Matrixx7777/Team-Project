a465e8a1bb73673be8b6e9e233fbc62653ea717e
mateusz-olszak <mateuszolszak@gmail.com>
2021-11-15 20:11:12 +0100

    Order Controller Implemenation

--- src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
@@ -1,21 +1,39 @@
 package com.kodilla.ecommercee.mapper;
 
+import com.kodilla.ecommercee.domain.Cart;
 import com.kodilla.ecommercee.domain.Order;
 import com.kodilla.ecommercee.dto.OrderDto;
+import com.kodilla.ecommercee.exceptions.CartNotFoundException;
+import com.kodilla.ecommercee.repository.CartRepository;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
-@AllArgsConstructor
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
 public class OrderMapper {
 
-    private CartMapper cartMapper;
+    private final CartRepository cartRepository;
+
+    public Order mapToOrder(OrderDto orderDto) throws CartNotFoundException {
+        Cart cart = cartRepository.findById(orderDto.getCartDtoId()).orElseThrow(CartNotFoundException::new);
+        return new Order(cart,orderDto.getOrderStatus());
+    }
 
     public OrderDto mapToOrderDto(Order order) {
         return new OrderDto(
                 order.getId(),
-                cartMapper.mapToCartDto(order.getCart()),
+                order.getCart().getId(),
                 order.getStatus()
         );
     }
+
+    public List<OrderDto> mapToOrderDtoList(List<Order> orders) {
+        return orders.stream().map(this::mapToOrderDto).collect(Collectors.toList());
+    }
 }
+
